ef6319dec727c82f884ffef2cc86f54858495687
Jean Letessier <83421070+Pixfri@users.noreply.github.com>
2024-11-15 09:03:17 +0100

    Initial commit

---
+++ Source/ProjectName/main.cpp
@@ -0,0 +1,7 @@
+#include "ProjectName/main.hpp"
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
---
+++ xmake.lua
@@ -0,0 +1,51 @@
+set_xmakever("2.9.3")
+
+ProjectName = "ProjectName"
+
+set_project(ProjectName)
+
+add_rules("mode.debug", "mode.release")
+set_languages("cxx20")
+
+option("override_runtime", {description = "Override VS runtime to MD in release and MDd in debug.", default = true})
+
+add_includedirs("Include")
+
+add_rules("plugin.vsxmake.autoupdate")
+
+if is_mode("release") then
+  set_fpmodels("fast")
+  set_optimize("fastest")
+  set_symbols("hidden")
+else
+  add_defines("PN_DEBUG")
+  set_symbols("debug")
+end
+
+set_encodings("utf-8")
+set_exceptions("cxx")
+set_languages("cxx20")
+set_rundir("./bin/$(plat)_$(arch)_$(mode)")
+set_targetdir("./bin/$(plat)_$(arch)_$(mode)")
+set_warnings("allextra")
+
+if is_plat("windows") then
+  if has_config("override_runtime") then
+    set_runtimes(is_mode("debug") and "MDd" or "MD")
+  end
+end
+
+add_cxflags("-Wno-missing-field-initializers -Werror=vla", {tools = {"clang", "gcc"}})
+
+target(ProjectName)
+  set_kind("binary")
+  
+  add_files("Source/**.cpp")
+  
+  for _, ext in ipairs({".hpp", ".inl"}) do
+    add_headerfiles("Include/**" .. ext)
+  end
+  
+  add_rpathdirs("$ORIGIN")
+
+includes("xmake/**.lua")
